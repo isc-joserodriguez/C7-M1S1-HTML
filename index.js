0b3fc01d9d5e2b02344a44f479d087cb3cdc58f4
José Rodriguez <isc.joserodriguez@gmail.com>
2022-04-30 12:52:05 -0600

    Segundo commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let otroNombre = 'hola'
+let variable2 = 'hola1'
+otroNombre = 'hola'
+
+otroNombre = 'hola'
+variable2 = 'Nuevo hola'
+
+otroNombre = 'hola'
+variable2 = 'Nuevo Otro hola'
+
+otroNombre = 'Otro hola'
+variable2 = 'Nuevo Otro hola'
+
+otroNombre = 'Otro hola'
+function hola() {
+}
+otroNombre = 'Otro hola'
+otroNombre = 'Otro hola'
+otroNo mbre = 'Otro hola'
+variable2 = 'Nuevo Otro hola'
+
+// TODO: Renombrar una variable
+// * Hola
+// ! asdfasf
+// ?---sds
